cc0e17e597bc3d81236c693f29b2a4ae1ea349bd
Nabil AIT TALEB <aittalebnabil@gmail.com>
2020-12-17 15:30:42 +0100

    Assuming blender is defined in the PATH system

--- scanner/vscanner_launch.sh
+++ scanner/vscanner_launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/usr/lib/python3.8
+# Launch blender python script (blender application must be defined in your PATH system)
 /opt/blender/blender -E CYCLES -b -noaudio -P vscanner_blender.py
