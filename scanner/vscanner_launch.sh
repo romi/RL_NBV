4b3b87de6d73282b9b6b0c97fef3944b4563c9bd
naittaleb <aittalebnabil@gmail.com>
2020-12-17 10:16:46 +0100

    Help launch Blender in Docker

--- scanner/vscanner_launch.sh
+++ scanner/vscanner_launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/usr/lib/python3.8
-/opt/blender/blender -b --python-use-system-env -P vscanner_blender.py
+#/opt/blender/blender -b --python-use-system-env -P vscanner_blender.py
+/opt/blender/blender -b -noaudio -P vscanner_blender.py
